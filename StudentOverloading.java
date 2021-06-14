7f1b9b368b57a25a0d40d5e501d0ba55e8ea834b
Kiran Vaddi <Kiran_Vaddi@epam.com>
2021-06-14 11:15:58 -0400

    problem added

---
+++ StudentOverloading.java
@@ -0,0 +1,14 @@
+public class StudentOverloading {
+    // properties id name marks
+
+    // default constructor
+    // id name
+    // id name marks
+
+    // getId()
+    // getId(name) => if name sent is equal to name in the objet then send the id or
+    // return 0
+    // getId(marks) => if marks sent is equal to marks in the objet then send the id
+    // or return 0
+
+}
